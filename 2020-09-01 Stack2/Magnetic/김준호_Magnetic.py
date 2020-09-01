ff6f093627a468077eb3fea4fabb2a81b1d6b8a2
juno0810 <juno0810@naver.com>
2020-09-01 14:14:03 +0900

    [UPDATE] 김준호 0901 주석

--- 2020-09-01 Stack2/Magnetic/김준호_Magnetic.py
+++ 2020-09-01 Stack2/Magnetic/김준호_Magnetic.py
@@ -2,17 +2,17 @@ T = 10
 
 def findStop(mat):
     count = 0
-    for i in range(100):
-        case = 0
-        for j in range(99):
-            if mat[j][i] == 1 and mat[j+1][i] == 2:
+    for i in range(100):    # 가로
+        case = 0            # 2가 나오기 전에 1이 나왔다면 1 아니라면 0
+        for j in range(99): # 세로
+            if mat[j][i] == 1 and mat[j+1][i] == 2: # 교착 상태
                 count += 1
                 case = 0
                 continue
-            if mat[j][i] == 1 and (mat[j+1][i] == 1 or mat[j+1][i] == 0):
+            if mat[j][i] == 1 and (mat[j+1][i] == 1 or mat[j+1][i] == 0):   # 1이 나온상황이므로 case 전환
                 case = 1
                 continue
-            if case == 1 and mat[j+1][i] == 2:
+            if case == 1 and mat[j+1][i] == 2:  # 1이 나온적이 있고 2가 나왔으므로 교착+1, case 초기화
                 case = 0 
                 count += 1
                 continue
--- 2020-09-01 Stack2/토너먼트카드게임/김준호_토너먼트카드게임.py
+++ 2020-09-01 Stack2/토너먼트카드게임/김준호_토너먼트카드게임.py
@@ -3,21 +3,23 @@ import sys
 sys.stdin = open("토너먼트카드게임_input.txt", "r")
 
 def winner(nums):
-    n = len(nums)
-    if n == 1:
-        return nums[0]
-    elif n == 2:
+    n = len(nums)   # 현재 카드 배열의 길이
+    if n == 1:      # 입력 카드 배열 길이가 1이면 그대로 반환
+        return nums[0]  # nums가 [(1,3)] 과 같은 형태이므로 값을 반환하기 위해 nums[0]
+    elif n == 2:    # 카드가 두개라면 비교!!
+        # 찌가 보를 이기는 상황을 제외하고는 모두 숫자가 크면 승리
         if max(nums[0], nums[1], key=lambda x: x[0])[0] == 3 and min(nums[0], nums[1], key=lambda x: x[0])[0] == 1:
-            return min(nums[0], nums[1], key=lambda x: x[0])
+            return min(nums[0], nums[1], key=lambda x: x[0])    # 순번이 아닌 카드 값으로 비교, 값이 같으면 앞에 있는 값이 반환
         else:
             return max(nums[0], nums[1], key=lambda x: x[0])
     else:
         return winner([winner(nums[:(n+1)//2]), winner(nums[(n+1)//2:])])
-        
+        # 규칙에 따라 반 갈라서 함수 재호출, ex) winner([(1,1), (2,3)]) 과 같은 형태가 됨
 T = int(input())
 
 for test_case in range(1, T + 1):
     n = int(input())
-    nums = list(zip(map(int, input().split()), range(1, n+1)))
-
+    nums = list(zip(map(int, input().split()), range(1, n+1)))  
+    # zip을 사용해서 각 카드에 번호 매겨주기
+    # ex) [(1,1),(3,2),(2,3),(1,4)]
     print('#{} {}'.format(test_case, winner(nums)[1]))
