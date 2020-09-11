59fe244160621cc054cb1767b99ddc76041e9bce
YeonggilGo <68373502+YeonggilGo@users.noreply.github.com>
2020-09-11 17:17:13 +0900

    Update 고영길_가능한시험점수.py

--- 2020-09-11 Tree prac/고영길_가능한시험점수.py
+++ 2020-09-11 Tree prac/고영길_가능한시험점수.py
@@ -10,3 +10,18 @@ for tc in range(1, int(input()) + 1):
         scores.update(set([score + point for score in scores]))
 
     print('#{} {}'.format(tc, len(scores)))
+
+    
+# I realized that the way to check if the score alreay exist in 'list' with the 'boolean list' is faster
+# than the way to check for duplicates through 'set'
+    scores2 = set([0])
+    cnt = 1 # bcuz of 0 point, count start from 1
+            # Through use 'cnt', we can skip the process which count the length of 'scores'
+    able_score = [False] * (sum(points) + 1)
+    for point in points:
+        for score in scores:
+            if not able_score[score + point]:
+                    able_score[score + point] = 1
+                    scores2.add(score + point)
+                    cnt += 1    
+        
