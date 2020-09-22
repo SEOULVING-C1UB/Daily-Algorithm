8d6ae369bcec7a708acd32fe982b0c0ad7f4ce2d
hue <euhmai@gmail.com>
2020-09-22 22:48:26 +0900

    [UPDATE] 최정휴 일곱닌젱이 주석수정

--- IM 대비 문제/일곱 난쟁이/최정휴_일곱난쟁이.py
+++ IM 대비 문제/일곱 난쟁이/최정휴_일곱난쟁이.py
@@ -8,15 +8,15 @@
  2) del쓸때 dwarfs[i] 먼저 제거하면 인덱스 꼬인다. 뒤부터 제거해라
 '''
 
-dwarfs = [int(input()) for _ in range(9)]
-dwarfs.sort()
+dwarfs = [int(input()) for _ in range(9)]               
+dwarfs.sort()                                           # 오름차순으로 뽑아야 하니까 미리 소트
 
-total_h = sum(dwarfs)
+total_h = sum(dwarfs)                                   # 9명의 총합을 미리 구해둠
 def find():
-    for i in range(8):
+    for i in range(8):                                  # 가짜 두명 찾는 이중반복문
         for j in range(i+1,9):
-            if total_h - dwarfs[i] - dwarfs[j] == 100:
-                del dwarfs[j]
+            if total_h - dwarfs[i] - dwarfs[j] == 100:  # 두명을 빼서 100이 되면 끝
+                del dwarfs[j]                           # j번째 지우고, i번째 지우고
                 del dwarfs[i]
                 return
 find()
