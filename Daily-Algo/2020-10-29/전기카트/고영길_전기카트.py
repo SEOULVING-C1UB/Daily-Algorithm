66801bf6254e7e5dffa9f6f223ca119b7020d6e4
yeonggilgo <dudrlf1859@naver.com>
2020-10-29 16:52:51 +0900

    [UPDATE] 고영길 1029 주석 + 수정

--- Daily-Algo/2020-10-29/전기카트/고영길_전기카트.py
+++ Daily-Algo/2020-10-29/전기카트/고영길_전기카트.py
@@ -4,9 +4,13 @@ for tc in range(1, int(input()) + 1):
     N = int(input())
     bat = [list(map(int, input().split())) for _ in range(N)]
     answer = 1000
+    # By using permutation, make the route like 1231, 1321.
+    # Because always start from 1, end from 1, exclude 1 from permutation.
     for per in pers(range(1, N), N - 1):
-        temp = bat[0][per[0]] + bat[per[-1]][0]
+        temp = bat[0][per[0]] + bat[per[-1]][0]  # 1-> second, second last -> 1
         for i in range(N - 2):
             temp += bat[per[i]][per[i + 1]]
+            if temp >= answer:  # back tracking
+                break
         answer = min(answer, temp)
     print('#{} {}'.format(tc, answer))
--- Daily-Algo/2020-10-29/컨테이너 운반/고영길_컨테이너 운반.py
+++ Daily-Algo/2020-10-29/컨테이너 운반/고영길_컨테이너 운반.py
@@ -1,13 +1,16 @@
+# Starting with heaviest cargo,
+# make sure that each cargo can be transported by each truck starting with largest truck.
+
 for tc in range(1, int(input()) + 1):
     N, M = map(int, input().split())
     weights = sorted(list(map(int, input().split())))
-    cap = sorted(list(map(int, input().split())))
+    trucks = sorted(list(map(int, input().split())))
     answer = 0
     for i in range(M - 1, -1, -1):
         try:
-            while cap[i] < weights[-1]:
+            while trucks[i] < weights[-1]:
                 weights.pop()
             answer += weights.pop()
-        except IndexError:
+        except IndexError: # no more cargo can be transported by left truck
             break
     print('#{} {}'.format(tc, answer))
